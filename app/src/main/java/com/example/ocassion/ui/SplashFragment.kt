a9e3c4bee1b198b03411a1b287c5773ef08dc37a
Vinilimaxx <vinicius.rodrigues@aluno.uepb.edu.br>
2024-04-04 22:12:02 -0300

    feature tela register and recover

--- app/src/main/java/com/example/ocassion/ui/SplashFragment.kt
+++ app/src/main/java/com/example/ocassion/ui/SplashFragment.kt
@@ -1,21 +1,44 @@
 package com.example.ocassion.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.ocassion.R
+import com.example.ocassion.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
 
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 1000)
+    }
+
+    private fun checkAuth(){
+        findNavController().navigate(R.id.loginFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/example/ocassion/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/ocassion/ui/auth/LoginFragment.kt
@@ -5,17 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.ocassion.R
+import com.example.ocassion.databinding.FragmentLoginBinding
+import com.example.ocassion.databinding.FragmentSplashBinding
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private fun initClicks() {
+        binding.btnRegister.setOnClickListener(){
+            findNavController().navigate(R.id.registerFragment)
+        }
+        binding.btnRecover.setOnClickListener(){
+            findNavController().navigate(R.id.recoverFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
