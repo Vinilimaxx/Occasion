1fc4f73a4b94e458149d3a4facdd97ab458aae26
Vinicius Rodrigues de Lima <91902651+Vinilimaxx@users.noreply.github.com>
2024-06-13 17:04:58 -0300

    Feature rollback v2 (#12)
    * commit inicial
    
    * feature tela register and recover
    
    * subindo
    
    * tela registro com firebase
    
    * telas auth completo
    
    * feat: tela auth com materializer
    
    * feat: board
    
    * bottom bar
    
    * tela-auth & botton-bar
    
    * feat: loading add
    
    * atual
    
    * fix: ui add fragment
    
    * feat: feature-text-post
    
    * fix: ajuste-feature-text-post
    
    * feat: navigationBtn
    
    * Criação da tela create_community
    
    * feat: functionBtn
    
    * fix: navigation
    
    * feat: post-firebase-home
    
    * fix:post-firebase-home
    
    * feature-prov-thome
    
    * feat: implementação da tela create e estilização de algumas telas.
    
    ---------
    
    Co-authored-by: felipebzs <andrefelipeb@hotmail.com>
    Co-authored-by: Felipebzs <97813270+Felipebzs@users.noreply.github.com>
    Co-authored-by: Natan Marques <natanmont123@gmail.com>

--- app/src/main/java/com/example/occasion/ui/AddFragment.kt
+++ app/src/main/java/com/example/occasion/ui/AddFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.occasion.R
+
 import com.example.occasion.databinding.FragmentAddBinding
 import com.example.occasion.model.Occasion
 import com.google.firebase.database.DatabaseReference
@@ -37,6 +39,10 @@ class AddFragment : Fragment() {
     private fun initClick() {
         binding.btnCancel.setOnClickListener {
             findNavController().popBackStack()
+        }
+
+        binding.btnCreate.setOnClickListener {
+            findNavController().navigate(R.id.action_menu_add_to_createCommunityFragment)
         }
 
         binding.btnPost.setOnClickListener {
